a8da786aea532bc45aea515bccd1431d450b9941
huang-yh <huangyh22@mails.tsinghua.edu.cn>
2023-08-31 16:24:12 +0800

    add linear mapping, especially for kitti

--- nerfstudio/fields/sdf_custom_field.py
+++ nerfstudio/fields/sdf_custom_field.py
@@ -19,7 +19,7 @@ Modified by Yuanhui Huang.
 
 import math
 from dataclasses import dataclass, field
-from typing import List, Optional, Type, Union
+from typing import List, Optional, Type, Union, Dict, Any
 
 import numpy as np
 import torch
@@ -39,6 +39,7 @@ from nerfstudio.field_components.encodings import (
 from nerfstudio.field_components.field_heads import FieldHeadNames
 from nerfstudio.field_components.spatial_distortions import SpatialDistortion
 from nerfstudio.fields.base_field import Field, FieldConfig
+from nerfstudio.configs.config_utils import to_immutable_dict
 
 try:
     import tinycudann as tcnn
@@ -50,99 +51,100 @@ except ImportError:
 # from . import cuda_gridsample_grad2.cuda_gridsample as cudagrid
 from .cuda_gridsample_grad2 import cuda_gridsample as cudagrid
 from .sh_render import SHRender
+from .mappings import GridMeterMapping
 
 
-class GridMeterMapping:
-    def __init__(
-        self,
-        bev_inner=128,
-        bev_outer=32,
-        range_inner=51.2,
-        range_outer=51.2,
-        nonlinear_mode="linear_upscale",
-        z_inner=20,
-        z_outer=10,
-        z_ranges=[-5.0, 3.0, 11.0],
-    ) -> None:
-        self.bev_inner = bev_inner
-        self.bev_outer = bev_outer
-        self.range_inner = range_inner
-        self.range_outer = range_outer
-        assert nonlinear_mode == "linear_upscale"  # TODO
-        self.nonlinear_mode = nonlinear_mode
-        self.z_inner = z_inner
-        self.z_outer = z_outer
-        self.z_ranges = z_ranges
+# class GridMeterMapping:
+#     def __init__(
+#         self,
+#         bev_inner=128,
+#         bev_outer=32,
+#         range_inner=51.2,
+#         range_outer=51.2,
+#         nonlinear_mode="linear_upscale",
+#         z_inner=20,
+#         z_outer=10,
+#         z_ranges=[-5.0, 3.0, 11.0],
+#     ) -> None:
+#         self.bev_inner = bev_inner
+#         self.bev_outer = bev_outer
+#         self.range_inner = range_inner
+#         self.range_outer = range_outer
+#         assert nonlinear_mode == "linear_upscale"  # TODO
+#         self.nonlinear_mode = nonlinear_mode
+#         self.z_inner = z_inner
+#         self.z_outer = z_outer
+#         self.z_ranges = z_ranges
 
-        self.hw_unit = range_inner * 1.0 / bev_inner
-        self.increase_unit = (range_outer - bev_outer * self.hw_unit) * 2.0 / bev_outer / (bev_outer + 1)
+#         self.hw_unit = range_inner * 1.0 / bev_inner
+#         self.increase_unit = (range_outer - bev_outer * self.hw_unit) * 2.0 / bev_outer / (bev_outer + 1)
 
-        self.z_unit = (z_ranges[1] - z_ranges[0]) * 1.0 / z_inner
-        self.z_increase_unit = (z_ranges[2] - z_ranges[1] - z_outer * self.z_unit) * 2.0 / z_outer / (z_outer + 1)
+#         self.z_unit = (z_ranges[1] - z_ranges[0]) * 1.0 / z_inner
+#         self.z_increase_unit = (z_ranges[2] - z_ranges[1] - z_outer * self.z_unit) * 2.0 / z_outer / (z_outer + 1)
 
-    def grid2meter(self, grid):
-        hw = grid[..., :2]
-        hw_center = hw - (self.bev_inner + self.bev_outer)
-        hw_center_abs = torch.abs(hw_center)
-        yx_base_abs = hw_center_abs * self.hw_unit
-        hw_outer = torch.relu(hw_center_abs - self.bev_inner)
-        hw_outer_int = torch.floor(hw_outer)
-        yx_outer_base = hw_outer_int * (hw_outer_int + 1) / 2.0 * self.increase_unit
-        yx_outer_resi = (hw_outer - hw_outer_int) * (hw_outer_int + 1) * self.increase_unit
-        yx_abs = yx_base_abs + yx_outer_base + yx_outer_resi
-        yx = torch.sign(hw_center) * yx_abs
+#     def grid2meter(self, grid):
+#         hw = grid[..., :2]
+#         hw_center = hw - (self.bev_inner + self.bev_outer)
+#         hw_center_abs = torch.abs(hw_center)
+#         yx_base_abs = hw_center_abs * self.hw_unit
+#         hw_outer = torch.relu(hw_center_abs - self.bev_inner)
+#         hw_outer_int = torch.floor(hw_outer)
+#         yx_outer_base = hw_outer_int * (hw_outer_int + 1) / 2.0 * self.increase_unit
+#         yx_outer_resi = (hw_outer - hw_outer_int) * (hw_outer_int + 1) * self.increase_unit
+#         yx_abs = yx_base_abs + yx_outer_base + yx_outer_resi
+#         yx = torch.sign(hw_center) * yx_abs
 
-        if grid.shape[-1] == 3:
-            d = grid[..., 2:3]
-            d_center = d
-            z_base = d_center * self.z_unit
+#         if grid.shape[-1] == 3:
+#             d = grid[..., 2:3]
+#             d_center = d
+#             z_base = d_center * self.z_unit
 
-            d_outer = torch.relu(d_center - self.z_inner)
-            d_outer_int = torch.floor(d_outer)
-            z_outer_base = d_outer_int * (d_outer_int + 1) / 2.0 * self.z_increase_unit
-            z_outer_resi = (d_outer - d_outer_int) * (d_outer_int + 1) * self.z_increase_unit
-            z = z_base + z_outer_base + z_outer_resi + self.z_ranges[0]
+#             d_outer = torch.relu(d_center - self.z_inner)
+#             d_outer_int = torch.floor(d_outer)
+#             z_outer_base = d_outer_int * (d_outer_int + 1) / 2.0 * self.z_increase_unit
+#             z_outer_resi = (d_outer - d_outer_int) * (d_outer_int + 1) * self.z_increase_unit
+#             z = z_base + z_outer_base + z_outer_resi + self.z_ranges[0]
 
-            return torch.cat([yx[..., 1:2], yx[..., 0:1], z], dim=-1)
-        else:
-            return yx[..., [1, 0]]
+#             return torch.cat([yx[..., 1:2], yx[..., 0:1], z], dim=-1)
+#         else:
+#             return yx[..., [1, 0]]
 
-    def meter2grid(self, meter):
-        xy = meter[..., :2]
-        xy_abs = torch.abs(xy)
-        wh_base_abs = xy_abs / self.hw_unit
-        wh_base_abs = wh_base_abs.clamp_(max=self.bev_inner)
-        xy_outer_abs = torch.relu(xy_abs - self.range_inner)
+#     def meter2grid(self, meter):
+#         xy = meter[..., :2]
+#         xy_abs = torch.abs(xy)
+#         wh_base_abs = xy_abs / self.hw_unit
+#         wh_base_abs = wh_base_abs.clamp_(max=self.bev_inner)
+#         xy_outer_abs = torch.relu(xy_abs - self.range_inner)
 
-        wh_outer_base = torch.sqrt(
-            (1.0 / 2 + self.hw_unit / self.increase_unit) ** 2 + 2 * xy_outer_abs / self.increase_unit
-        ) - (1.0 / 2 + self.hw_unit / self.increase_unit)
-        wh_outer_base = torch.floor(wh_outer_base)
-        xy_outer_resi = (
-            xy_outer_abs - wh_outer_base * self.hw_unit - self.increase_unit * wh_outer_base * (wh_outer_base + 1) / 2
-        )
-        wh_outer_resi = xy_outer_resi / (self.hw_unit + (wh_outer_base + 1) * self.increase_unit)
-        wh_center_abs = wh_base_abs + wh_outer_base + wh_outer_resi
-        wh_center = torch.sign(xy) * wh_center_abs
-        wh = wh_center + self.bev_inner + self.bev_outer
+#         wh_outer_base = torch.sqrt(
+#             (1.0 / 2 + self.hw_unit / self.increase_unit) ** 2 + 2 * xy_outer_abs / self.increase_unit
+#         ) - (1.0 / 2 + self.hw_unit / self.increase_unit)
+#         wh_outer_base = torch.floor(wh_outer_base)
+#         xy_outer_resi = (
+#             xy_outer_abs - wh_outer_base * self.hw_unit - self.increase_unit * wh_outer_base * (wh_outer_base + 1) / 2
+#         )
+#         wh_outer_resi = xy_outer_resi / (self.hw_unit + (wh_outer_base + 1) * self.increase_unit)
+#         wh_center_abs = wh_base_abs + wh_outer_base + wh_outer_resi
+#         wh_center = torch.sign(xy) * wh_center_abs
+#         wh = wh_center + self.bev_inner + self.bev_outer
 
-        z = meter[..., 2:3]
-        z_abs = z - self.z_ranges[0]
-        d_base = z_abs / self.z_unit
-        d_base = d_base.clamp_(max=self.z_inner)
-        z_outer = torch.relu(z_abs - (self.z_ranges[1] - self.z_ranges[0]))
+#         z = meter[..., 2:3]
+#         z_abs = z - self.z_ranges[0]
+#         d_base = z_abs / self.z_unit
+#         d_base = d_base.clamp_(max=self.z_inner)
+#         z_outer = torch.relu(z_abs - (self.z_ranges[1] - self.z_ranges[0]))
 
-        d_outer_base = torch.sqrt(
-            (1.0 / 2 + self.z_unit / self.z_increase_unit) ** 2 + 2 * z_outer / self.z_increase_unit
-        ) - (1.0 / 2 + self.z_unit / self.z_increase_unit)
-        d_outer_base = torch.floor(d_outer_base)
-        z_outer_resi = (
-            z_outer - d_outer_base * self.z_unit - self.z_increase_unit * d_outer_base * (d_outer_base + 1) / 2
-        )
-        d_outer_resi = z_outer_resi / (self.z_unit + (d_outer_base + 1) * self.z_increase_unit)
-        d = d_base + d_outer_base + d_outer_resi
+#         d_outer_base = torch.sqrt(
+#             (1.0 / 2 + self.z_unit / self.z_increase_unit) ** 2 + 2 * z_outer / self.z_increase_unit
+#         ) - (1.0 / 2 + self.z_unit / self.z_increase_unit)
+#         d_outer_base = torch.floor(d_outer_base)
+#         z_outer_resi = (
+#             z_outer - d_outer_base * self.z_unit - self.z_increase_unit * d_outer_base * (d_outer_base + 1) / 2
+#         )
+#         d_outer_resi = z_outer_resi / (self.z_unit + (d_outer_base + 1) * self.z_increase_unit)
+#         d = d_base + d_outer_base + d_outer_resi
 
-        return torch.cat([wh[..., 1:2], wh[..., 0:1], d], dim=-1)
+#         return torch.cat([wh[..., 1:2], wh[..., 0:1], d], dim=-1)
 
 
 class LaplaceDensity(nn.Module):  # alpha * Laplace(loc=0, scale=beta).cdf(-sdf)
@@ -282,14 +284,29 @@ class SDFCustomFieldConfig(FieldConfig):
     """whether to use smoothstep for multi-resolution hash grids"""
     use_position_encoding: bool = True
     """whether to use positional encoding as input for geometric network"""
-    bev_inner: int = 128
-    bev_outer: int = 32
-    range_inner: float = 51.2
-    range_outer: float = 51.2
-    nonlinear_mode: str = "linear_upscale"
-    z_inner: int = 20
-    z_outer: int = 10
-    z_ranges: List[float] = field(default_factory=lambda: [-5.0, 3.0, 11.0])
+
+    mapping_args: Dict[str, Any] = to_immutable_dict(
+        dict(
+            nonlinear_mode="linear_upscale",
+            h_size=[128, 32],
+            h_range=[51.2, 28.8],
+            h_half=False,
+            w_size=[128, 32],
+            w_range=[51.2, 28.8],
+            w_half=False,
+            d_size=[20, 10],
+            d_range=[-4.0, 4.0, 12.0],
+        )
+    )
+
+    # bev_inner: int = 128
+    # bev_outer: int = 32
+    # range_inner: float = 51.2
+    # range_outer: float = 51.2
+    # nonlinear_mode: str = "linear_upscale"
+    # z_inner: int = 20
+    # z_outer: int = 10
+    # z_ranges: List[float] = field(default_factory=lambda: [-5.0, 3.0, 11.0])
     # mlp decoder
     embed_dims: int = 128
     color_dims: int = 0
@@ -323,18 +340,22 @@ class SDFCustomField(Field):
         self.config = config
 
         # custom configs
-        self.z_size = self.config.z_inner + self.config.z_outer + 1
-        self.bev_size = 2 * (self.config.bev_inner + self.config.bev_outer) + 1
+        # self.z_size = self.config.z_inner + self.config.z_outer + 1
+        # self.bev_size = 2 * (self.config.bev_inner + self.config.bev_outer) + 1
         self.mapping = GridMeterMapping(
-            self.config.bev_inner,
-            self.config.bev_outer,
-            self.config.range_inner,
-            self.config.range_outer,
-            self.config.nonlinear_mode,
-            self.config.z_inner,
-            self.config.z_outer,
-            self.config.z_ranges,
+            # self.config.bev_inner,
+            # self.config.bev_outer,
+            # self.config.range_inner,
+            # self.config.range_outer,
+            # self.config.nonlinear_mode,
+            # self.config.z_inner,
+            # self.config.z_outer,
+            # self.config.z_ranges,
+            **self.config.mapping_args
         )
+        self.bev_size = [self.mapping.size_h, self.mapping.size_w]
+        self.z_size = self.mapping.size_d
+
         self.color_converter = SHRender
         self.sh_deg = self.config.sh_deg
         self.sh_act = self.config.sh_act
@@ -394,7 +415,7 @@ class SDFCustomField(Field):
 
     def pre_compute_density_color(self, bev, dtype=torch.float):
         assert bev.dim() == 3
-        bev = bev.unflatten(1, (self.bev_size, self.bev_size))
+        bev = bev.unflatten(1, self.bev_size)
         density_color = self.density_net(bev).reshape(*bev.shape[:-1], self.z_size, -1)
         density_color = density_color.permute(0, 4, 1, 2, 3)  # bs, C, h, w, d
         self.density_color = density_color.to(dtype)
@@ -402,10 +423,10 @@ class SDFCustomField(Field):
 
     def forward_geonetwork(self, inputs):
         """forward the geonetwork"""
-        grid = self.mapping.meter2grid(inputs)
+        grid = self.mapping.meter2grid(inputs, True)
 
-        grid[..., :2] = grid[..., :2] / (self.bev_size - 1)
-        grid[..., 2:] = grid[..., 2:] / (self.z_size - 1)
+        # grid[..., :2] = grid[..., :2] / (self.bev_size - 1)
+        # grid[..., 2:] = grid[..., 2:] / (self.z_size - 1)
         grid = 2 * grid - 1
         grid = grid.reshape(1, -1, 1, 1, 3)
 
@@ -422,10 +443,10 @@ class SDFCustomField(Field):
 
     def forward_sdfnetwork(self, inputs):
         """forward the geonetwork"""
-        grid = self.mapping.meter2grid(inputs)
+        grid = self.mapping.meter2grid(inputs, True)
 
-        grid[..., :2] = grid[..., :2] / (self.bev_size - 1)
-        grid[..., 2:] = grid[..., 2:] / (self.z_size - 1)
+        # grid[..., :2] = grid[..., :2] / (self.bev_size - 1)
+        # grid[..., 2:] = grid[..., 2:] / (self.z_size - 1)
         grid = 2 * grid - 1
         grid = grid.reshape(1, -1, 1, 1, 3)
 
